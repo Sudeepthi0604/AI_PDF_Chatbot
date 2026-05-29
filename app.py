19d1d12c64d5104a1c01e81cca7c302b9c826ed0
Sudeepthi barla <sudeepthibarla2005@gmail.com>
2026-05-29 18:36:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,8 @@ import time
 st.set_page_config(page_title="AI PDF Chatbot", layout="wide")
 
 API_KEY = "AIzaSyCPfuMTrwD9Kt_lswrZq1I5RItX1cfZhdo"
-client = genai.Client(api_key=API_KEY)
-
-
+genai.configure(api_key=API_KEY)
+model = genai.GenerativeModel("gemini-1.5-flash")
 # DATABASE INIT
 
 def init_db():
