1a0ef329ee358edecb1b46207c33f6dc105a262c
Sudeepthi barla <sudeepthibarla2005@gmail.com>
2026-05-29 19:00:46 +0530

    Update API key assignment in app.py
    Replace API_KEY with hardcoded GOOGLE_API_KEY.

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import time
 # =====================
 st.set_page_config(page_title="AI PDF Chatbot", layout="wide")
 
-API_KEY = st.secrets["GOOGLE_API_KEY"]
+GOOGLE_API_KEY = "AIzaSyCPfuMTrwD9Kt_lswrZq1I5RItX1cfZhdo"
 genai.configure(api_key=API_KEY)
 
 model = genai.GenerativeModel("gemini-1.5-flash")
